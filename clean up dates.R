a424b8ddb9b9c4b2c9edc4e8fe60501f170e31a3
Bartesto <barthuntley@iinet.net.au>
2015-04-01 10:44:34 +0800

    construct date fields

--- clean up dates.R
+++ clean up dates.R
@@ -1,14 +1,22 @@
 rm(list=ls())
 
+library(lubridate)
+
 setwd("Z:\\DOCUMENTATION\\BART\\R\\R_DEV\\marine_science\\Marine-Science")
 
 ## get data in
-df <- read.table("data\\Coral.txt", skip = 1, stringsAsFactors = FALSE)
-a <- paste0(df[1,15], " ", df[1,16], " ", df[1,17])
-df[,15] <- a
-df <- df[,1:15]
+dfr <- read.table("data\\Coral.txt", skip = 1, stringsAsFactors = FALSE,
+                 colClasses = c(rep("character", 8), rep("numeric", 6),
+                                rep("character", 3)))
+a <- paste0(dfr[1,15], " ", dfr[1,16], " ", dfr[1,17])
+dfr[,15] <- a
+df <- dfr[,1:15]
 rnames <- c("BYYY", "BM", "BD", "BH", "EYYY", "EM", "ED", "EH", "SST", "SSTANOM",
             "HOTSPOT", "DHW", "Lat", "Long", "Reef_Name")
 names(df) <- rnames
 
-
+## dates
+bd <- ymd(paste0(df[,1], df[,2], df[,3]))
+ed <- ymd(paste0(df[,5], df[,6], df[,7]))
+df$Bdate <- bd
+df$Edate <- ed
